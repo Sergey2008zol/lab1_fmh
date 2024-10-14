e3e4aae80483eef44bc8cafcccb85453ad943d1e
Sergey2008zol <sjzolotov@gmail.com>
2024-10-15 00:23:44 +0300

    Update programm

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-
+#include <math.h>  
+double f(double x)
+{
+    double y;
+    y=log(x);
+    return y;
+}
+double ploshad(double a, double b, double E)
+{
+    int elem=1000;
+    double S=0;
+    for( ;(b-a)/elem>E;elem*=2){}
+    double h=(b-a)/elem;
+    //printf("%d ", elem);
+    //printf("%lf ", h);
+    double x1=a+h,x=a;
+    for( ;x1<b;x1+=h)
+    {
+        S+=((f(x)+f(x1))*h)/2;
+        //printf("%lf ", S);
+        x=x1;
+    }
+    return S;
+}
 int main()
 {
-    printf("Hello, World! My name Meto");
-
+    double a,b,E;
+    scanf("%lf %lf %lf", &a, &b, &E);
+    printf("%lf", ploshad(a,b,E));
     return 0;
 }
